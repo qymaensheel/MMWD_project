3c335c0768acc516bf2661d8311bbcc4d139f6a2
Bartosz Nieroda <bartosz@nieroda.pl>
2021-01-17 17:59:29 +0100

    added importing from file v2

--- client.py
+++ client.py
@@ -4,19 +4,18 @@ import random
 class Client:
     occupied_ID = []
 
-    def __init__(self, lower_needs, upper_needs):
-        if not self.occupied_ID:
-            self.ID = 1
-        else:
-            self.ID = self.occupied_ID[-1] + 1
-        self.occupied_ID.append(self.ID)
+    def __init__(self, lower_needs, upper_needs, provided=False):
+        if not provided:                #random data
+            if not self.occupied_ID:
+                self.ID = 1
+            else:
+                self.ID = self.occupied_ID[-1] + 1
+            self.occupied_ID.append(self.ID)
 
-        self.needs = random.randint(lower_needs, upper_needs)
-        self.restaurants = dict()
+            self.needs = random.randint(lower_needs, upper_needs)
+        else:                           #predefined data
+            self.ID = lower_needs
+            self.occupied_ID.append(self.ID)
+            self.needs = upper_needs
 
-    def __init__(self, id_, needs_, provided):      # musi byc dodatkowy parametr bo inaczej konstruktory beda
-        # przyjmowac taka sama ilosc argumentow i beda sie przyslaniac
-        self.ID = id_
-        self.occupied_ID.append(self.ID)
-        self.needs = needs_
         self.restaurants = dict()
--- restaurant.py
+++ restaurant.py
@@ -4,18 +4,18 @@ import random
 class Restaurant:
     occupied_ID = []
 
-    def __init__(self, lower_distance, upper_distance, lower_cost, upper_cost):
-        if not self.occupied_ID:
-            self.ID = 1
+    def __init__(self, lower_distance, upper_distance, lower_cost, upper_cost = -1):
+        if upper_cost== -1:                     #predefined data
+            self.ID = lower_distance
+            self.occupied_ID.append(self.ID)
+            self.max_distance = upper_distance
+            self.cost = lower_cost
         else:
-            self.ID = self.occupied_ID[-1] + 1
-        self.occupied_ID.append(self.ID)
-        self.max_distance = random.randint(lower_distance, upper_distance)
-        self.cost = random.randint(lower_cost, upper_cost)
-        self.clients = dict()
-
-    def __init__(self, id_, d_, c_):
-        self.ID = id_
-        self.occupied_ID.append(self.ID)
-        self.max_distance = d_
-        self.cost = c_
+            if not self.occupied_ID:
+                self.ID = 1
+            else:                                #random data
+                self.ID = self.occupied_ID[-1] + 1
+            self.occupied_ID.append(self.ID)
+            self.max_distance = random.randint(lower_distance, upper_distance)
+            self.cost = random.randint(lower_cost, upper_cost)
+            self.clients = dict()
